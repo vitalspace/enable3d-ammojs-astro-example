7748e679938ded3919302f36aaddb6dfa467320e
vitalspace <myglobalsounds@gmail.com>
2023-05-28 21:55:59 -0600

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vitalspace.github.io',
+    site: 'https://vitalspace.github.io/enable3d-ammojs-astro-example/',
     base: '/enable3d-ammojs-astro-example/',
 });
